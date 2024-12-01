63aaed709fa28b6f6f08de08fbbfa34a7fcc9f98
Davis, Jack William <jdavis647@gatech.edu>
2024-12-01 13:10:05 -0500

    Updates

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -20,7 +20,7 @@ const Resume = () => {
                 desc={val.desc} />
               )
             }
-            return;
+            return null;
           })}
         </div>
 
@@ -36,7 +36,7 @@ const Resume = () => {
                   desc={val.desc} />
               )
             }
-            return;
+            return null;
           })}
         </div>
 
